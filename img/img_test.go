0cbce2b22be3702bbefac9b95b5f660779158d6d
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-05-30 20:20:21 +1200

    chore(img): improve test converage

--- img/img_test.go
+++ img/img_test.go
@@ -1,33 +1,83 @@
-package img_test
+package img
 
 import (
 	"testing"
-
-	"github.com/scottmckendry/pat/img"
 )
 
 func TestDecode(t *testing.T) {
-	_, err := img.Decode("./test_img.png")
+	_, err := Decode("./test_img.png")
 	if err != nil {
 		t.Fatalf("Error decoding image: %v", err)
 	}
 }
 
 func TestDecodeBadPath(t *testing.T) {
-	_, err := img.Decode("./bad_path.png")
+	_, err := Decode("./bad_path.png")
 	if err == nil {
 		t.Fatalf("Expected error decoding image")
 	}
 }
 
 func TestResize(t *testing.T) {
-	image, err := img.Decode("./test_img.png")
+	image, err := Decode("./test_img.png")
 	if err != nil {
 		t.Fatalf("Error decoding image: %v", err)
 	}
 
-	image = img.Resize(image, 80, 0)
+	image = Resize(image, 80, 0)
 	if image.Bounds().Dx() != 80 {
 		t.Fatalf("Expected width to be 80, got %d", image.Bounds().Dx())
+	}
+}
+
+func TestDecodeFromUrl(t *testing.T) {
+	_, err := decodeFromUrl("https://scottmckendry.tech/img/logo.webp")
+	if err != nil {
+		t.Fatalf("Error decoding image from URL: %v", err)
+	}
+}
+
+func TestDecodeFromUrlBadUrl(t *testing.T) {
+	_, err := decodeFromUrl("https://scottmckendry.tech/img/bad_url.webp")
+	if err == nil {
+		t.Fatalf("Expected error decoding image from URL")
+	}
+}
+
+func TestDecodeFromFile(t *testing.T) {
+	_, err := decodeFromFile("./test_img.png")
+	if err != nil {
+		t.Fatalf("Error decoding image from file: %v", err)
+	}
+}
+
+func TestDecodeFromFileBadPath(t *testing.T) {
+	_, err := decodeFromFile("./bad_path.png")
+	if err == nil {
+		t.Fatalf("Expected error decoding image from file")
+	}
+}
+
+func TestIsUrl(t *testing.T) {
+	if !isUrl("http://example.com") {
+		t.Fatalf("Expected URL to be recognized")
+	}
+}
+
+func TestIsUrlBadUrl(t *testing.T) {
+	if isUrl("example.com") {
+		t.Fatalf("Expected URL to not be recognized")
+	}
+}
+
+func TestPathExists(t *testing.T) {
+	if !pathExists("./test_img.png") {
+		t.Fatalf("Expected path to exist")
+	}
+}
+
+func TestPathExistsBadPath(t *testing.T) {
+	if pathExists("./bad_path.png") {
+		t.Fatalf("Expected path to not exist")
 	}
 }
